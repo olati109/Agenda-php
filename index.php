09277aba233d88173e2b6278616d2dab1ae95107
italo <italocoelho109@gmail.com>
2022-02-12 23:50:08 -0300

    terminado a home do projeto

--- index.php
+++ index.php
@@ -1,27 +1,40 @@
 <?php
-
-include_once("config/url.php");
-
+include_once("templates/header.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Agenda de contato</title>
-  <!-- BOOTSTRAP -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- FONT-AWESOME -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- CSS -->
-  <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
-</head>
-
-<body>
-  <h1>Testando Agenda</h1>
-  <i class="fas fa-eye"></i>
-</body>
-
-</html>
+<div class="container">
+  <?php if (isset($printMsg) && $printMsg != '') : ?>
+    <p id="msg"> <?= $printMsg ?></p>
+  <?php endif; ?>
+  <h1 id="main-title">Minha Agenda</h1>
+  <?php if (count($contacts) > 0) : ?>
+    <table class="table" id="contacts-table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Telefone</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($contacts as $contact) : ?>
+          <tr>
+            <td scope="row" class="col-id"> <?= $contact["id"] ?></td>
+            <td scope="row"> <?= $contact["name"] ?></td>
+            <td scope="row"> <?= $contact["phone"] ?></td>
+            <td class="actions">
+              <a href="#"><i class="fas fa-eye check-icon"></i></a>
+              <a href="#"><i class="far fa-edit edit-icon"></i></a>
+              <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  <?php else : ?>
+    <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">clique aqui para adicionar</a>.</p>
+  <?php endif; ?>
+</div>
+<?php
+include_once("templates/footer.php");
+?>
---
+++ templates/header.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once("config/url.php");
+include_once("config/process.php");
+
+//limpa a mensagem
+if (isset($_SESSION['msg'])) {
+  $printMsg = $_SESSION['msg'];
+  $_SESSION['msg'] = '';
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Agenda de contato</title>
+  <!-- BOOTSTRAP -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- FONT-AWESOME -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- CSS -->
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+</head>
+
+<body>
+
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+        <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+      </a>
+      <div>
+        <div class="navbar-nav">
+          <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>index.php">Agenda</a>
+          <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>create.php">Adicionar contato</a>
+        </div>
+      </div>
+    </nav>
+  </header>
